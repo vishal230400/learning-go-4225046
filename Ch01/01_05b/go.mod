69ff217d76a5e38c9e73b6e91ab7a064c5d52b5d
Vishal Sankar Ram <vishals230400@gmail.com>
2025-07-13 01:55:09 +0000

    1

---
+++ Ch01/01_05b/go.mod
@@ -0,0 +1,3 @@
+module com.example.01_05b
+
+go 1.22.12
--- Ch02/02_02b/main.go
+++ Ch02/02_02b/main.go
@@ -6,9 +6,9 @@ import (
 
 func main() {
 
-	// str1 := "The quick red fox"
-	// str2 := "jumped over"
-	// str3 := "the lazy brown dog."
-
-	fmt.Println("Hello from Go!")
+	str1 := "The quick red fox"
+	str2 := "jumped over"
+	str3 := "the lazy brown dog."
+	int1 := 10
+	fmt.Println(str1, int1, str2, str3)
 }
